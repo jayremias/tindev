87c550787d11ebec8a0444a1221181f628cfd820
Jonathan Bertoldi <contato@jonathanbertoldi.com.br>
2020-02-25 02:05:56 -0300

    create like and dislike functionalities and relactive routes; Add cors package.

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,5 +1,42 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
 module.exports = {
-    store(req, res) {
-        return res.json({ok: true});
+    async index(req, res) {
+        const { user } = req.headers;
+        
+        const loggedUser = await Dev.findById(user);
+        
+        const users = await Dev.find({
+            $and: [
+                { _id: { $ne: user } },
+                { _id: { $nin: loggedUser.likes } },
+                { _id: { $nin: loggedUser.dislikes } },
+            ]
+        });
+        
+        res.json(users);
+    },
+    async store(req, res) {
+        const { username } = req.body;
+
+        const userExists = await Dev.findOne({ user: username });
+
+        if(userExists) {
+            return res.json(userExists);
+        }
+        
+        response = await axios.get(`https://api.github.com/users/${username}`);
+        
+        const { avatar_url:avatar, name, bio } = response.data;
+
+        const dev = await Dev.create({
+            name,
+            user: username,
+            bio,
+            avatar
+        });
+        
+        return res.json(dev);
     }
 };
